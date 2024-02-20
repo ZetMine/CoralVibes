f45edf6da8925b15f05efa0468d253db6eca5a8b
ZetMine <99437373+ZetMine@users.noreply.github.com>
2024-02-20 18:10:21 +0100

    PlayerSlotChange fix

--- src/main/java/net/zetmine/coralvibes/listeners/PlayerSlotChange.java
+++ src/main/java/net/zetmine/coralvibes/listeners/PlayerSlotChange.java
@@ -1,28 +1,22 @@
 package net.zetmine.coralvibes.listeners;
 
-import net.zetmine.coralvibes.utils.ItemBuilder;
 import net.zetmine.coralvibes.utils.Items;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryMoveItemEvent;
-import org.bukkit.event.player.PlayerItemHeldEvent;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
 public class PlayerSlotChange implements Listener {
 
     private final Items items = new Items();
 
     @EventHandler
-    public void onPlayerItemHeld(InventoryMoveItemEvent event) {
-        ItemStack mainMenu = items.mainMenu;
-        ItemStack shop = items.shop;
-
-        if (event.getItem().equals(mainMenu) || event.getItem().equals(shop)){
-            event.setCancelled(true);
-        }
+    public void onPlayerItemHeld(InventoryClickEvent event) {
+        event.setCancelled(true);
+    }
+    @EventHandler
+    public void onSwapItemHand(PlayerSwapHandItemsEvent event) {
+        event.setCancelled(true);
     }
 
 }
