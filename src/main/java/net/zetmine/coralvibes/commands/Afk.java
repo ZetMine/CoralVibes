af82314deb95b655bdf7cf17ef1b0b57018e1b38
n0thingme <this.n0thingme@Gmail.com>
2024-02-23 12:01:15 +0100

    Truc wolf là afk

--- src/main/java/net/zetmine/coralvibes/commands/Afk.java
+++ src/main/java/net/zetmine/coralvibes/commands/Afk.java
@@ -1,12 +1,14 @@
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import net.zetmine.coralvibes.utils.Utils;
 import org.bukkit.entity.Player;
 
 public class Afk implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Utils utils = new Utils();
         // Vérifie si la commande est exécutée par un joueur
         if (!(sender instanceof Player)) {
             sender.sendMessage("Cette commande ne peut être utilisée que par un joueur.");
@@ -16,7 +18,7 @@ public class Afk implements CommandExecutor {
         Player player = (Player) sender;
 
         // Votre logique pour gérer la commande /afk ici
-        player.sendMessage(ChatColor.GREEN +"Vous êtes maintenant AFK.");
+        player.sendMessage(utils.cvPrefix + ChatColor.GREEN +"Vous êtes maintenant AFK.");
 
         return true;
     }
